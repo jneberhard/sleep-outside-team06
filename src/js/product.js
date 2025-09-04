de6850832a5a2a5901bc774feeaf56fafab6198f
jneberhard <jreberhard3@gmail.com>
2025-09-04 17:27:39 -0600

    jne branch - fix cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart") || []; //get existing cart
+  cart.push(product); //adds the new product
+  setLocalStorage("so-cart", cart); //saves the updated cart
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
